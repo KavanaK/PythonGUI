7edcd3e0b1a34cd3c9403a40151d9f2785aa3e4f
Kavana.K <kavana.kamila@gmail.com>
2019-07-25 18:38:04 +0530

    further changes and little more code

--- Pingpong.py
+++ Pingpong.py
@@ -10,6 +10,12 @@ win.bgcolor("black")
 win.setup(width=800,height=600)
 win.tracer(0)
 
+
+#Main game loop
+
+#while True:
+  #  win.update()
+
 #setting score
 
 score_a = 0
@@ -19,6 +25,10 @@ score_b = 0
 #paddle A
 
 paddle_a = turtle.Turtle()
-paddle_a.speed(0)
+paddle_a.speed(10)
 paddle_a.shape("square")
 paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5 , stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
