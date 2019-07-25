3268e077d5f681c75d3776b30e1264ecb182f9da
Kavana.K <kavana.kamila@gmail.com>
2019-07-25 17:39:42 +0530

    first paddle is added

--- Pingpong.py
+++ Pingpong.py
@@ -9,3 +9,16 @@ win.title("Pong")
 win.bgcolor("black")
 win.setup(width=800,height=600)
 win.tracer(0)
+
+#setting score
+
+score_a = 0
+score_b = 0
+
+
+#paddle A
+
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
